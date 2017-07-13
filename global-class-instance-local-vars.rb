5b28a251d2a07eaa166534f9cb8fbbf656f249a4
Chyld Medford <chyld.medford@gmail.com>
2017-07-12 21:34:39 -0700

    variables

---
+++ global-class-instance-local-vars.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+$g1 = 3
+
+### ----------------------------------------- ###
+
+# GLOBAL VARIABLES
+
+def temp1
+    $g1 * 5
+end
+
+p temp1
+
+### ----------------------------------------- ###
+
+# INSTANCE AND CLASS VARIABLES
+
+class Dog
+    @@counter = 0
+
+    def self.counter
+        @@counter
+    end
+
+    def initialize(name)
+        @name = name
+        @@counter += 1
+    end
+
+    def bark
+        "woof, i am #{@name}"
+    end
+end
+
+d1 = Dog.new('fido')
+d2 = Dog.new('sam')
+d3 = Dog.new('lassy')
+
+### ----------------------------------------- ###
+
+# binding.pry
